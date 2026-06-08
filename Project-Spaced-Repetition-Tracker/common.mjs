273ca7399f8b38798ae95abed5c189a4a6afd245
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-06-08 22:31:23 +0100

    Add calculateRevisionDate function and corresponding tests

--- Project-Spaced-Repetition-Tracker/common.mjs
+++ Project-Spaced-Repetition-Tracker/common.mjs
@@ -1,3 +1,29 @@
 export function getUserIds() {
   return ["1", "2", "3", "4", "5"];
 }
+
+// Step 1: Create date calculation function (Rubric requires this to be in a separate function)
+// Input: a date
+// Output: an array of 5 dates
+
+export function calculateRevisionDate(inputDate) {
+  const oneWeekLater = new Date(inputDate);
+  oneWeekLater.setDate(oneWeekLater.getDate() + 7);
+  const oneMonthLater = new Date(inputDate);
+  oneMonthLater.setMonth(oneMonthLater.getMonth() + 1);
+  const threeMonthsLater = new Date(inputDate);
+  threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+  const sixMonthsLater = new Date(inputDate);
+  sixMonthsLater.setMonth(sixMonthsLater.getMonth() + 6);
+  const oneYearLater = new Date(inputDate);
+  oneYearLater.setFullYear(oneYearLater.getFullYear() + 1);
+
+  // Returns: [week, month, 3months, 6months, 1year]
+  return [
+    oneWeekLater,
+    oneMonthLater,
+    threeMonthsLater,
+    sixMonthsLater,
+    oneYearLater,
+  ];
+}
--- Project-Spaced-Repetition-Tracker/common.test.mjs
+++ Project-Spaced-Repetition-Tracker/common.test.mjs
@@ -1,7 +1,31 @@
+import { calculateRevisionDate } from "./common.mjs";
 import { getUserIds } from "./common.mjs";
 import assert from "node:assert";
 import test from "node:test";
 
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
+});
+
+test("Revision dates are correct", () => {
+  assert.equal(
+    calculateRevisionDate("2026-07-19")[0].toISOString().split("T")[0],
+    "2026-07-26",
+  );
+  assert.equal(
+    calculateRevisionDate("2026-07-19")[1].toISOString().split("T")[0],
+    "2026-08-19",
+  );
+  assert.equal(
+    calculateRevisionDate("2026-07-19")[2].toISOString().split("T")[0],
+    "2026-10-19",
+  );
+  assert.equal(
+    calculateRevisionDate("2026-07-19")[3].toISOString().split("T")[0],
+    "2027-01-19",
+  );
+  assert.equal(
+    calculateRevisionDate("2026-07-19")[4].toISOString().split("T")[0],
+    "2027-07-19",
+  );
 });
