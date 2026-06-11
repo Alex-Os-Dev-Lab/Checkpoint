9ff82d304c5534c74a7b267c3fba4582e192a86b
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-06-11 19:26:46 +0100

    Refactor index.html and script.mjs: clean up placeholder comments and ensure proper meta tags are included

--- Project-Spaced-Repetition-Tracker/script.mjs
+++ Project-Spaced-Repetition-Tracker/script.mjs
@@ -1,12 +1,8 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
+// import functions from the other modules
 import { getUserIds } from "./common.mjs";
+import { calculateRevisionDate } from "./common.mjs";
+import { getUpcomingAgendaItems } from "./common.mjs";
+import { getData, addData } from "./storage.mjs";
 
-window.onload = function () {
-  const users = getUserIds();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
-};
+// add event listeners and main logic here to interact with the DOM
+// and use the functions from common.mjs and storage.mjs
